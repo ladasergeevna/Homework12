1a8a536bd4c3ffa52772b374a0be444c3cc31400
Lada <ladasergeevna@mail.ru>
2024-11-09 21:35:26 +0900

    toString, equals, hashCode

--- src/Author.java
+++ src/Author.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Author {
     String firstName;
     String lastName;
@@ -14,5 +16,20 @@ public class Author {
     public String getLastName() {
         return this.lastName;
     }
+    public String toString() {
+        return this.firstName + " " + this.lastName;
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(firstName, author.firstName) && Objects.equals(lastName, author.lastName);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,23 @@ public class Main {
         Author gogol = new Author("Николай", "Гоголь");
         Author coelho = new Author("Пауло", "Коэльо");
         Book deadSouls = new Book("Мертвые души", gogol, 1842);
+        Book deadSoulsTom2 = new Book("Мертвые души", gogol, 1842);
         Book alchemist = new Book("Алхимик", coelho, 1988);
         System.out.println("Название книги:" + deadSouls.getTitle() + ", Автор:" + deadSouls.getAuthor().getFirstName() + " " + deadSouls.getAuthor().getLastName() + ", Год издания:" + deadSouls.getYearPublication());
         System.out.println("Название книги:" + alchemist.getTitle() + ", Автор:" + alchemist.getAuthor().getFirstName() + " " + alchemist.getAuthor().getLastName() + ", Год издания:" + alchemist.getYearPublication());
-        deadSouls.setYearPublication(1843);
-        System.out.println("Название книги:" + deadSouls.getTitle() + ", Автор:" +
-                "" + deadSouls.getAuthor().getFirstName() + " " + deadSouls.getAuthor().getLastName() + ", Год издания:" + deadSouls.getYearPublication());
+        alchemist.setYearPublication(1989);
+        System.out.println("Название книги:" + alchemist.getTitle() + ", Автор:" + alchemist.getAuthor().getFirstName() + " " + alchemist.getAuthor().getLastName() + ", Год издания:" + alchemist.getYearPublication());
+
+        System.out.println();
+        System.out.println("Homework_2");
+        System.out.println(gogol.toString());
+        System.out.println(deadSouls.toString());
+        System.out.println(deadSoulsTom2.toString());
+        System.out.println("Гоголь = Коэльо? " + gogol.equals(coelho));
+        System.out.println("Мертвые Души = Мертвые души.Том 2? " + deadSouls.equals(deadSoulsTom2));
+        System.out.println(alchemist.hashCode());
+
     }
+
+
 }
